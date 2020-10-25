dfcd47acbb9b1844c6015965e47a54562f548ee1
jeffa-bob <brody.tyler.46@gmail.com>
2020-10-25 15:20:53 -0700

    add port

--- GoReceive/main.go
+++ GoReceive/main.go
@@ -15,9 +15,9 @@ func main() {
  `), "", 600, 200)
 	defer ui.Close()
 	// Create a GoLang function callable from JS
-	ui.Bind("hello", func() string { return "World!" })
+	//ui.Bind("hello", func() string { return "World!" })
 	// Call above `hello` function then log to the JS console
-	ui.Eval("hello().then( (x) => { console.log(x) })")
+	//ui.Eval("hello().then( (x) => { console.log(x) })")
 	// Wait until UI window is closed
 	<-ui.Done()
 }
