5e81c594659dce970e50347f9697937de7b1d036
jeffa-bob <brody.tyler.46@gmail.com>
2020-10-25 13:53:10 -0700

    added webView and Lorca

--- GoReceive/main.go
+++ GoReceive/main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
-	"fyne.io/fyne/app"
-	"fyne.io/fyne/widget"
-	"github.com/zach-klippenstein/goadb"
+	"github.com/zserge/lorca"
+	"net/url"
 )
 
 func main() {
-	adb.Adb
+	// Create UI with data URI
+	ui, _ := lorca.New("data:text/html,"+url.PathEscape(`
+ <html>
+  <head><title>Hello</title></head>
+  <body><h1>Hello, world!</h1></body>
+ </html>
+ `), "", 600, 200)
+	defer ui.Close()
+	// Create a GoLang function callable from JS
+	ui.Bind("hello", func() string { return "World!" })
+	// Call above `hello` function then log to the JS console
+	ui.Eval("hello().then( (x) => { console.log(x) })")
+	// Wait until UI window is closed
+	<-ui.Done()
 }
