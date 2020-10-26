0737226101e94cdb147c883a390c2aa470cb19ee
jeffa-bob <brody.tyler.46@gmail.com>
2020-10-26 08:09:04 -0700

    fixed html

--- GoRemote/main.go
+++ GoRemote/main.go
@@ -2,50 +2,27 @@ package main
 
 import (
 	"net/url"
-
-	"github.com/zserge/lorca"
+    "github.com/zserge/lorca"
+    "github.com/webview/webview"
 )
 
-const selectpage = `
-<html>
-
-<head>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
-    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-</head>
-
-<body>
-    <form action="#">
-        <div style="margin-left: 2%" class="mdl-textfield mdl-js-textfield">
-            <input class="mdl-textfield__input" type="text"
-                pattern="((^\s*((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*$)|(^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$))"
-                id="sample2">
-            <label class="mdl-textfield__label" for="sample2">Address</label>
-            <span class="mdl-textfield__error">Input is not a Address!</span>
-        </div>
-    </form>
-    <form action="#">
-        <div style="margin-left: 2%" class="mdl-textfield mdl-js-textfield">
-            <input class="mdl-textfield__input" type="text"
-                pattern="^([0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$"
-                id="sample3">
-            <label class="mdl-textfield__label" for="sample2">Port</label>
-            <span class="mdl-textfield__error">Input is not a Port!</span>
-        </div>
-    </form>
-</body>
-
-</html>`
-
-func main() {
+func lorcaMain() {
 	// Create UI with data URI
-	ui, _ := lorca.New("data:text/html,"+url.PathEscape(selectpage), "", 600, 200)
+	ui, _ := lorca.New("data:text/html,"+url.PathEscape(formhtml), "", 600, 200)
 	defer ui.Close()
-	// Create a GoLang function callable from JS
-	//ui.Bind("hello", func() string { return "World!" })
-	// Call above `hello` function then log to the JS console
-	//ui.Eval("hello().then( (x) => { console.log(x) })")
-	// Wait until UI window is closed
 	<-ui.Done()
+}
+
+func main(){
+    webviewMain()
+    //lorcaMain()
+}
+
+func webviewMain(){
+    w := webview.New(true)
+    defer w.Destroy()
+    w.SetSize(310, 200, webview.HintNone)
+    w.Navigate("data:text/html,"+url.PathEscape(formhtml))
+     
+    w.Run()
 }
